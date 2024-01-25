3bfa8980bb5006c6f1c940fcde93466be95b2b83
dongjiuzhu1 <dongjiuzhu1@xiaomi.com>
2024-01-25 22:42:53 +0800

    external/mtp-responder: reset g_is_send_object to resume next transfer
    Signed-off-by: dongjiuzhu1 <dongjiuzhu1@xiaomi.com>

--- src/mtp_cmd_handler.c
+++ src/mtp_cmd_handler.c
@@ -44,7 +44,7 @@ extern pthread_mutex_t g_cmd_inoti_mutex;
 extern mtp_config_t g_conf;
 
 mtp_bool g_is_sync_estab = FALSE;
-static mtp_bool g_is_send_object = FALSE;
+mtp_bool g_is_send_object = FALSE;
 
 /*
  * STATIC VARIABLES
