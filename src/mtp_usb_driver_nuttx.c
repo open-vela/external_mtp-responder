c1e201120aed45c0cf8195e10ee4f7a0f7e3eb41
wangjianyu3 <wangjianyu3@xiaomi.com>
2024-04-19 21:03:12 +0800

    mtp-responder: Send setup response if in state ONSERVICE
    Signed-off-by: wangjianyu3 <wangjianyu3@xiaomi.com>

--- src/mtp_usb_driver_nuttx.c
+++ src/mtp_usb_driver_nuttx.c
@@ -362,10 +362,12 @@ static void __handle_control_request(mtp_int32 request)
             sent_busy = FALSE;
         }
 
-        status = write(g_usb_ep0, &statusreq_data, sizeof(statusreq_data));
-        if (status < 0) {
-            ERR("IOCTL MTP_SEND_STATUS_ACK Failed [%d]\n",
-                errno);
+        if (_transport_get_mtp_operation_state() == MTP_STATE_ONSERVICE) {
+            status = write(g_usb_ep0, &statusreq_data, sizeof(statusreq_data));
+            if (status < 0) {
+                ERR("IOCTL MTP_SEND_STATUS_ACK Failed [%d]\n",
+                    errno);
+            }
         }
         break;
 
