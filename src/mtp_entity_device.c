d6ef3d7a06e4127771aa69fa8a6ee00eedcebfe2
wangjianyu3 <wangjianyu3@xiaomi.com>
2024-10-21 22:46:17 +0800

    mtp-responder: Disable device icon by default
    Signed-off-by: wangjianyu3 <wangjianyu3@xiaomi.com>

--- src/mtp_entity_device.c
+++ src/mtp_entity_device.c
@@ -368,10 +368,12 @@ static mtp_bool __init_device_props()
 	i++;
 #endif /*MTP_SUPPORT_DEVICE_CLASS*/
 
+#ifdef MTP_SUPPORT_DEVICE_ICON
 	dev_prop = &(g_device.device_prop_list[i]);
 	_prop_init_device_property_desc(dev_prop, MTP_PROPERTYCODE_DEVICEICON,
 			PTP_DATATYPE_AUINT8, PTP_PROPGETSET_GETONLY, NONE);
 	i++;
+#endif /*MTP_SUPPORT_DEVICE_ICON*/
 
 	already_init = TRUE;
 
